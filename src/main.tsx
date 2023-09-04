62ab2b5c33e5ccdcc6889a89c7528b90451d809b
Michael Feher <michael.feher@algorand.foundation>
2023-09-04 08:04:26 -0500

    chore: bump packages for testing, add import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/main.css'
 import ErrorBoundary from './components/ErrorBoundary'
-
+import * as algokit from '@algorandfoundation/algokit-utils'
+console.log(algokit)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ErrorBoundary>
